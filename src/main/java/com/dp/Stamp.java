d55619502e0a1c61cf1079a7e1fd55c323959def
piyush <piyush2k13@gmail.com>
2014-07-03 21:50:38 +0530

    uploading file with signature

---
+++ src/main/java/com/dp/Stamp.java
@@ -0,0 +1,24 @@
+package com.dp;
+
+/*
+ * Firstly, Need to find longest length stamp, l
+ * Secondly, Need to find color change count along desirecColor, colorChange
+ * result = l * stampCost + colorChange * pushCost
+ */
+
+public class Stamp {
+
+	int getMinimumCost(String desiredColor, int stampCost, int pushCost) {
+		return 0;
+	}
+	
+	public static void main(String[] args) {
+		String desiredColor = "RRGGBB";
+		int stampCost = 1;
+		int pushCost = 1;
+		
+		System.out.println(new Stamp().getMinimumCost(desiredColor, stampCost, pushCost));
+
+	}
+
+}
