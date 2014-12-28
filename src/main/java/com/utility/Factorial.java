e9fbea0359022e83f66c1be01192ef2a5a30a6c6
piyush <piyush2k13@gmail.com>
2014-12-28 10:36:53 +0530

    Added Complete String solution

--- src/main/java/com/utility/Factorial.java
+++ src/main/java/com/utility/Factorial.java
@@ -5,19 +5,15 @@ public class Factorial {
 	public static long getFactorial(long number) {
 		long factorial = 1;
 		
-		for(long i = 2; i < number + 1; i++) {
-			
+		for(long i = 1; i <= number; i++) {
 			factorial = i * factorial;
-			
 		}
-		
-		
 		return factorial;
 	}
 	
 	public static void main(String[] args) {
 		
-		System.out.println(getFactorial(0));
+		System.out.println(getFactorial(50));
 	}
 
 }
