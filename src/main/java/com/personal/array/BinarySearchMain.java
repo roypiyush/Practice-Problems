0214ea3d280e09f689a9992b1cbece65b4fc5866
piyush <piyush2k13@gmail.com>
2016-01-13 21:23:38 +0530

    added lower and upper bound algorithm for binary search

--- src/main/java/com/personal/array/BinarySearchMain.java
+++ src/main/java/com/personal/array/BinarySearchMain.java
@@ -27,10 +27,52 @@ public class BinarySearchMain {
 			return mid;
 
 	}
+	
+	int upperBound(int arr[], int key) {
+		
+		int low = 0; int high = arr.length - 1; int mid = 0;
+		
+		while(low <= high) {
+			mid = (low + high) >> 1;
+			
+			if(arr[mid] > key && (mid == 1 || arr[mid - 1] <= key)) {
+				return mid;
+			}
+			else if(arr[mid] > key) {
+				high = mid - 1;
+			}
+			else {
+				low = mid + 1;
+			}
+		}
+		
+		return mid;
+	}
+	
+	int lowerBound(int arr[], int key) {
+		
+		int low = 0; int high = arr.length - 1; int mid = 0;
+		
+		while(low <= high) {
+			mid = (low + high) >> 1;
+			
+			if(arr[mid] >= key && (mid == 1 || arr[mid - 1] < key)) {
+				return mid;
+			}
+			else if(arr[mid] >= key) {
+				high = mid - 1;
+			}
+			else {
+				low = mid + 1;
+			}
+		}
+		
+		return mid;
+	}
 
 	public static void main(String[] args) {
 
-		int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
+		int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 16,
 				17, 18, 19, 20 };
 
 		BinarySearchMain main = new BinarySearchMain();
@@ -41,6 +83,14 @@ public class BinarySearchMain {
 		System.out.println((index = main.binarySearch(arr, 0, arr.length - 1, 0)) != -1 ? "Element found at " + index : "Element not Found!");
 		System.out.println((index = main.binarySearch(arr, 0, arr.length - 1, 1)) != -1 ? "Element found at " + index : "Element not Found!");
 		System.out.println((index = main.binarySearch(arr, 0, arr.length - 1, 20)) != -1 ? "Element found at " + index : "Element not Found!");
+		
+		int upperBound = 1;
+		index = main.upperBound(arr, upperBound);
+		System.out.println(String.format("Upper bound found for key:%d at index:%d element:%d", upperBound, index, arr[index]));
+		
+		int lowerBound = 13;
+		index = main.lowerBound(arr, lowerBound);
+		System.out.println(String.format("Lower bound found for key:%d at index:%d element:%d", lowerBound, index, arr[index]));
 	}
 
 }
