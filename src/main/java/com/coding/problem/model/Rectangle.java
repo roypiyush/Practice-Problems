6ed47f9daa8a7944d40938b857f2900600d3e957
Piyush Roy <piyush2k13@gmail.com>
2017-06-12 21:04:01 +0530

    added crazy rectangles problem

---
+++ src/main/java/com/coding/problem/model/Rectangle.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package com.coding.problem.model;
+
+/**
+ * @author piyush
+ *
+ */
+public class Rectangle {
+
+	private Point leftBottom;
+	private Point rightTop;
+
+	public Point getLeftBottom() {
+		return leftBottom;
+	}
+	public void setLeftBottom(Point leftBottom) {
+		this.leftBottom = leftBottom;
+	}
+	public Point getRightTop() {
+		return rightTop;
+	}
+	public void setRightTop(Point rightTop) {
+		this.rightTop = rightTop;
+	}
+	public int area() {
+		return (rightTop.getX() - leftBottom.getX()) * (rightTop.getY() - leftBottom.getY());
+	}
+	@Override
+	public String toString() {
+		return "Rectangle [leftBottom=" + leftBottom + ", rightTop=" + rightTop + "]";
+	}
+	
+	
+}
