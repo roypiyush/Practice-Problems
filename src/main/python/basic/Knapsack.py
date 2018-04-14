ed464e2ce5b4adc8c766fccfec798e58b85da749
piyush <piyush2k13@gmail.com>
2018-04-14 15:08:48 +0530

    fixed knapsack algorithm

--- src/main/python/basic/Knapsack.py
+++ src/main/python/basic/Knapsack.py
@@ -9,19 +9,18 @@ def knapsack(weight_arr, value_arr, w):
 
     max_value = -1000
     for i in range(len(weight_arr)):
-        rw = weight_arr[i] - w
-        max_value = max(int(max_value), int(value_arr[i]) + int(knapsack(weight_arr, value_arr, rw)))
+        rw = w - weight_arr[i]
+        max_value = max(max_value, value_arr[i] + knapsack(weight_arr, value_arr, rw))
     return max_value
 
 
-val = [30, 100, 120]
-wt = [10, 20, 30]
-W = 50
-
-result_arr = list()
-result_arr.append(0)
-result_arr.append(-1)
-result_arr.append(-1)
-result_arr.append(-1)
-
-print (knapsack(wt, val, W))
+if __name__ == '__main__':
+    val = [30, 100, 120]
+    wt = [10, 20, 30]
+    W = 50
+    result_arr = list()
+    result_arr.append(0)
+    result_arr.append(-1)
+    result_arr.append(-1)
+    result_arr.append(-1)
+    print (knapsack(wt, val, W))
