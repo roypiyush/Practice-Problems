a436b194d5159acc14b6d7c3da6f7b0aa784609a
Piyush <piyush@email.com>
2018-09-25 21:47:35 +0530

    minor fixes

--- src/main/python/basic/compare_sorting_algo.py
+++ src/main/python/basic/compare_sorting_algo.py
@@ -6,7 +6,7 @@ import random
 
 def main():
 
-    print "Creating data samples"
+    print("Creating data samples")
     t_start = datetime.now()
 
     limit = 100000
@@ -14,7 +14,7 @@ def main():
     for i in range(0, limit):
         array.append(random.randint(1, limit * 100))
 
-    print "Starting algorithms"
+    print("Starting algorithms")
     # Start of Quick Sort
     q_start = datetime.now()
     quick_sort.quick_sort(array, 0, len(array) - 1)
@@ -35,10 +35,10 @@ def main():
 
     t_done = datetime.now()
 
-    print "Quick Sort Time taken %d ms" % ((q_done - q_start).microseconds / 1000)
-    print "Merge Sort Time taken %d ms" % ((m_done - m_start).microseconds / 1000)
-    print "Heap Sort Time taken %d ms" % ((h_done - h_start).microseconds / 1000)
-    print "Total Time taken %d sec" % (t_done - t_start).seconds
+    print("Quick Sort Time taken %d ms" % ((q_done - q_start).microseconds / 1000))
+    print("Merge Sort Time taken %d ms" % ((m_done - m_start).microseconds / 1000))
+    print("Heap Sort Time taken %d ms" % ((h_done - h_start).microseconds / 1000))
+    print("Total Time taken %d sec" % (t_done - t_start).seconds)
 
 
 if __name__ == '__main__':
@@ -46,4 +46,4 @@ if __name__ == '__main__':
     try:
         main()
     except Exception as e:
-        print "Caught Exception %s" % e.message
+        print("Caught Exception %s" % e.message)
