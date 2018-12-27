ca54577751edbb82a8d42ff4d31e14fabc65ceff
Piyush <piyush@email.com>
2018-12-27 15:56:40 +0530

    added dfs

--- src/main/python/graph/graph.py
+++ src/main/python/graph/graph.py
@@ -16,9 +16,12 @@ class Vertex:
         self.adj_list = []
         self.distance = None
         self.parent = None
+        self.start_time = None
+        self.end_time = None
 
     def __str__(self):
-        return "Vertex[{}] >>> color={}, distance={}".format(self.key, self.color, self.distance)
+        return "Key={} Color={} D={} st={} et={}".format(self.key, self.color,
+                                                         self.distance, self.start_time, self.end_time)
 
 
 class Graph:
@@ -31,8 +34,9 @@ class Graph:
         if type(vertices) != dict:
             raise TypeError(colored("Graph expected to be dictionary of vertices", 'blue'))
         self.vertices = vertices
+        self.time = 0
 
-    def bfs_traversal(self, start_key):
+    def breath_first_search(self, start_key):
         queue = []
         vertex = self.vertices[start_key]
 
@@ -51,8 +55,28 @@ class Graph:
                     print(colored("{} ".format(u), 'magenta'))
             v.color = Color.BLACK
 
+    def depth_first_search(self):
+        for v in self.vertices.values():
+            if v.color == Color.WHITE:
+                self.__depth_first_visit(v)
 
-if __name__ == '__main__':
+    def __depth_first_visit(self, vertex):
+        self.time += 1
+        vertex.start_time = self.time
+        vertex.color = Color.GRAY
+        for v in vertex.adj_list:
+            if v.color == Color.WHITE:
+                self.__depth_first_visit(v)
+        self.time += 1
+        vertex.end_time = self.time
+        vertex.color = Color.BLACK
+
+    def print_vertices(self):
+        for v in self.vertices.values():
+            print(v)
+
+
+def __main__():
     v1 = Vertex(1)
     v2 = Vertex(2)
     v3 = Vertex(3)
@@ -63,15 +87,20 @@ if __name__ == '__main__':
     v8 = Vertex(8)
     v9 = Vertex(9)
     v10 = Vertex(10)
-
     v1.adj_list = [v2, v3, v4]
     v2.adj_list = [v5, v6]
     v3.adj_list = [v7, v8]
     v4.adj_list = [v9, v10]
     v6.adj_list = [v9]
     v7.adj_list = [v10]
+    vertices = dict({1: v1, 2: v2, 3: v3, 4: v4, 5: v5, 6: v6, 7: v7, 8: v8, 9: v9, 10: v10})
+    graph = Graph(vertices)
+    graph.breath_first_search(1)
+    for v in vertices.values():
+        v.color = Color.WHITE
+    graph.depth_first_search()
+    graph.print_vertices()
 
-    vertices_main = dict({1: v1, 2: v2, 3: v3, 4: v4, 5: v5, 6: v6, 7: v7, 8: v8, 9: v9, 10: v10})
 
-    graph = Graph(vertices_main)
-    graph.bfs_traversal(1)
+if __name__ == '__main__':
+    __main__()
