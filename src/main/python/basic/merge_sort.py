608586cde98f368073d782566a6ba3817048ef17
Piyush <piyush2k13@gmail.com>
2019-05-01 13:30:32 +0530

    refactored

--- src/main/python/basic/merge_sort.py
+++ src/main/python/basic/merge_sort.py
@@ -1,41 +1,41 @@
-
 def merge(array, i, mid, j):
-    left_array = array[i: mid + 1]
-    right_array = array[mid + 1: j + 1]
-    l_size = len(left_array)
-    r_size = len(right_array)
-    # merge two sorted arrays
+    l_arr = array[i: mid + 1]
+    r_arr = array[mid + 1: j + 1]
     left_index = 0
     right_index = 0
-    p = i
-    inversion_count = 0
-    while p <= j:
-        if left_index < l_size and right_index < r_size and left_array[left_index] <= right_array[right_index]:
-            array[p] = left_array[left_index]
-            left_index = left_index + 1
-        elif left_index < l_size and right_index < r_size and right_array[right_index] < left_array[left_index]:
-            array[p] = right_array[right_index]
-            right_index = right_index + 1
-            inversion_count += (l_size - left_index)
-        elif left_index < l_size:
-            array[p] = left_array[left_index]
-            left_index = left_index + 1
-        elif right_index < r_size:
-            array[p] = right_array[right_index]
-            right_index = right_index + 1
-        p = p + 1
-    return inversion_count
+    left_size = len(l_arr)
+    right_size = len(r_arr)
+    k = i
+    inversions = 0
+    while left_index < left_size or right_index < right_size:
+        if right_index < right_size and left_index < left_size and l_arr[left_index] <= r_arr[right_index]:
+            array[k] = l_arr[left_index]
+            k += 1
+            left_index += 1
+        elif right_index < right_size and left_index < left_size and r_arr[right_index] < l_arr[left_index]:
+            array[k] = r_arr[right_index]
+            k += 1
+            right_index += 1
+            inversions += (left_size - left_index)
+        elif right_index == right_size:
+            array[k] = l_arr[left_index]
+            k += 1
+            left_index += 1
+        elif left_index == left_size:
+            array[k] = r_arr[right_index]
+            k += 1
+            right_index += 1
+    return inversions
 
 
 def merge_sort(array, i, j):
-    inversion_count = 0
     if i < j:
         mid = (i + j) >> 1
-        x = merge_sort(array, i, mid)
-        y = merge_sort(array, mid + 1, j)
-        z = merge(array, i, mid, j)
-        return x + y + z
-    return inversion_count
+        left_inversion = merge_sort(array, i, mid)
+        right_inversion = merge_sort(array, mid + 1, j)
+        merged_inversion = merge(array, i, mid, j)
+        return left_inversion + right_inversion + merged_inversion
+    return 0
 
 
 def naive_inversion_counting(array):
@@ -51,20 +51,18 @@ def naive_inversion_counting(array):
 
 
 def main():
-    limit = 100
+    limit = 1000
     unsorted_array = []
     for i in range(0, limit):
         import random
         unsorted_array.append(random.randint(1, limit * 100))
-    print("Generated Sequence ", unsorted_array)
-    print("Naive inversion count ", naive_inversion_counting(unsorted_array))
+    naive_inversions = naive_inversion_counting(unsorted_array)
     from datetime import datetime
     start = datetime.now()
     inversions = merge_sort(unsorted_array, 0, len(unsorted_array) - 1)
     done = datetime.now()
-    if __name__ == '__main__':
-        print('Inversions = ', inversions)
-        print("Time taken %s seconds" % str((done - start).seconds))
+    print("Naive Inversion {}\nMerge Inversion {} ".format(naive_inversions, inversions))
+    print("Time taken for merge sort %s seconds" % str((done - start).total_seconds()))
     import asserter as a
     a.assert_sorted(unsorted_array)
 
